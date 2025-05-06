a96a5b18f5e017e72cd43c393684fc51ff185b7f
Tal Arbetov <talarbatov98@gmail.com>
2025-05-07 01:21:01 +0300

    socket cleanup

--- app/socket.ts
+++ app/socket.ts
@@ -1,8 +1,5 @@
 import { io } from 'socket.io-client';
 import { apiConfig } from '../api-config'
-// "undefined" means the URL will be computed from the `window.location` object
 
-export const socket = io(apiConfig.uri, {
-    // withCredentials: true
-});
+export const socket = io(apiConfig.uri);
 
